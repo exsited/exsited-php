936cc2b4004e9398363b82800a46fc017cc621ca
shuvo <shuvo.a@webalive.com.au>
2025-06-02 23:33:08 +1000

    partnerFunctionDisable, partnerFunctionEnable, readIntegrationConfiguration

--- src/AppService/Integration/IntegrationData.php
+++ src/AppService/Integration/IntegrationData.php
@@ -74,4 +74,48 @@ class IntegrationData
             throw new AutoBillApiException($e->getMessage());
         }
     }
+
+    public function partnerFunctionDisable($integrationUuid, $partnerFunctionUuid, $apiVersion = null)
+    {
+        $attribute = "partner-function/$partnerFunctionUuid/disable";
+        try {
+            $requestBuilder = new AutoBillRequestBuilder($this->apiConfig->getAuthCredentialData());
+            $apiVersion = $apiVersion ?? SdkVersionManager::getApiVersion();
+            return $requestBuilder->callResourceAttribute(ApiResource::INTEGRATION, AutoBillApiSchemeHelper::POST, $integrationUuid, null, $attribute, $apiVersion);
+        } catch (AutoBillApiException $e) {
+            throw new AutoBillApiException($e->getMessage());
+        }
+    }
+
+    public function partnerFunctionEnable($integrationUuid, $partnerFunctionUuid, $apiVersion = null)
+    {
+        $attribute = "partner-function/$partnerFunctionUuid/enable";
+        try {
+            $requestBuilder = new AutoBillRequestBuilder($this->apiConfig->getAuthCredentialData());
+            $apiVersion = $apiVersion ?? SdkVersionManager::getApiVersion();
+            return $requestBuilder->callResourceAttribute(ApiResource::INTEGRATION, AutoBillApiSchemeHelper::POST, $integrationUuid, null, $attribute, $apiVersion);
+        } catch (AutoBillApiException $e) {
+            throw new AutoBillApiException($e->getMessage());
+        }
+    }
+
+    public function readIntegrationConfiguration($integrationUuid, $apiVersion = null)
+    {
+        $attribute = "configuration";
+        try {
+            $requestBuilder = new AutoBillRequestBuilder($this->apiConfig->getAuthCredentialData());
+            $apiVersion = $apiVersion ?? SdkVersionManager::getApiVersion();
+            return $requestBuilder->callResourceAttribute(
+                ApiResource::INTEGRATION,
+                AutoBillApiSchemeHelper::GET,
+                $integrationUuid,
+                null,
+                $attribute,
+                $apiVersion
+            );
+        }
+        catch (AutoBillApiException $e) {
+            throw new AutoBillApiException($e->getMessage());
+        }
+    }
 }
